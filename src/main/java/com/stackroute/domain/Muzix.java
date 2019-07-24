4cae84677c90e6cebad3ae409687662e13f4bbbb
Sahithi <sahithimpc@gmail.com>
2019-07-24 17:01:37 +0530

    Commiting task9 in springboot

--- src/main/java/com/stackroute/domain/Muzix.java
+++ src/main/java/com/stackroute/domain/Muzix.java
@@ -1,10 +1,19 @@
 package com.stackroute.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 //Maps to the muzix class
 @Entity
+//Data annotation in lombok
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+//It will create all getters and setters and also constructors
 public class Muzix {
     //PrimaryKey in the table
     @Id
@@ -12,47 +21,5 @@ public class Muzix {
     private String name;
     private String track;
 
-    //Default and parameterized constructors
-    public Muzix() {
-    }
 
-    public Muzix(int id, String name, String track) {
-        this.id = id;
-        this.name = name;
-        this.track = track;
-    }
-
-    //Getter and setter methods
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTrack() {
-        return track;
-    }
-
-    public void setTrack(String track) {
-        this.track = track;
-    }
-
-    @Override
-    public String toString() {
-        return "Muzix{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", track='" + track + '\'' +
-                '}';
-    }
 }
