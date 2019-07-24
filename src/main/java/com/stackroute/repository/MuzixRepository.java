c56c0b2610c40307966cdf69bbfce711313560b0
Sahithi <sahithimpc@gmail.com>
2019-07-24 14:56:06 +0530

    Commiting task4 of spring boot

--- src/main/java/com/stackroute/repository/MuzixRepository.java
+++ src/main/java/com/stackroute/repository/MuzixRepository.java
@@ -2,10 +2,15 @@ package com.stackroute.repository;
 
 import com.stackroute.domain.Muzix;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 //Repository which extends the Jpa Repository
 @Repository
 public interface MuzixRepository extends JpaRepository<Muzix,Integer> {
-
+    //Query annotation for retrieving data
+    @Query(value = "select m from Muzix m where m.name=?1")
+    public List<Muzix> trackByName(String name);
 }
