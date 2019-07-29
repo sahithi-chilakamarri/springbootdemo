f265b519214e7cbcd50f8aa71d4ba23cfb4822b3
sahithi-chilakamarri <51943021+sahithi-chilakamarri@users.noreply.github.com>
2019-07-29 18:02:24 +0530

    Update MuzixService.java

--- src/main/java/com/stackroute/service/MuzixService.java
+++ src/main/java/com/stackroute/service/MuzixService.java
@@ -11,16 +11,17 @@ import java.util.Optional;
 //Service interface
 public interface MuzixService {
 //We have to implement all the methods in the implementation class
+    //To save the track
     public Muzix saveTrack(Muzix muzix) throws TrackAlreadyExistsException;
-
+     //To get all the tracks
     public List<Muzix> getAllTracks();
-
+     //To update the track
     public Muzix updateTrack(int trackId,Muzix muzix);
-
+     //To delete the track
     public List<Muzix> deleteTrack(int trackId) throws TrackNotFoundException;
-
+     //To get the toptracks in api
     public void getTopTracks();
-
+    //To get the tracks by name
     public List<Muzix> getTracksByName(String name);
 
 
