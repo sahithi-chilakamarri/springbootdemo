3afca4cefa5d641f499dfa49f193efc5459235dd
sahithi-chilakamarri <51943021+sahithi-chilakamarri@users.noreply.github.com>
2019-07-29 17:54:48 +0530

    Update MuzixController.java

--- src/main/java/com/stackroute/controller/MuzixController.java
+++ src/main/java/com/stackroute/controller/MuzixController.java
@@ -18,43 +18,53 @@ public class MuzixController {
     //Creating object for the muzix service
     private MuzixService muzixService;
 
-    //Constructor
+    //Parameterized Constructor
     public MuzixController(MuzixService muzixService) {
         this.muzixService = muzixService;
     }
 
-    //Saving the track
+    //Saving the track 
     @PostMapping("muzix")
+    //Handler method for savinvg the track
     public ResponseEntity<?> saveMuzix(@RequestBody Muzix muzix) throws TrackAlreadyExistsException {
         ResponseEntity responseEntity;
         //Saving the track
         muzixService.saveTrack(muzix);
+        //HttpStatus Code
         responseEntity = new ResponseEntity("successfully created", HttpStatus.CREATED);
         return responseEntity;
     }
 
     //Retrieving all the tracks
     @GetMapping("muzix")
+    //Handler method for getting the tracks
     public ResponseEntity<?> getAllMuzix(String url, Class<Muzix> muzixClass) {
         //To get all the tracks from the api and store it to the database.
         muzixService.getTopTracks();
+        //HttpStatus code
         return new ResponseEntity(muzixService.getAllTracks(), HttpStatus.OK);
     }
 
     //Deleting the track
     @DeleteMapping("muzix")
+    //Handler method for the delete tracks
     public ResponseEntity<?> deleteMuzix(@RequestBody Muzix muzix) throws TrackNotFoundException {
+        //HttpStatusCode
         return new ResponseEntity(muzixService.deleteTrack(muzix.getTrackId()), HttpStatus.OK);
     }
 
     //Updating the track
     @PutMapping("muzix")
+    //Handler method for the updating tracks
     public ResponseEntity<?> updateMuzix(@RequestBody Muzix muzix) {
+        //HttpStatus Code
         return new ResponseEntity<>(muzixService.updateTrack(muzix.getTrackId(), muzix), HttpStatus.OK);
     }
     //Getting all the tracks matched by the name
     @GetMapping("trackByName")
+    //Handler method for the track by name
     public ResponseEntity<?> getTrackByName(@RequestParam String name) throws TrackNotFoundException {
+        //HttpStatus code
         return new ResponseEntity<List<Muzix>>(muzixService.getTracksByName(name), HttpStatus.OK);
     }
 }
