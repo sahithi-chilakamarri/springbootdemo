1d039e76609214ff3bdea01a072ac9254b8d4ee4
sahithi-chilakamarri <51943021+sahithi-chilakamarri@users.noreply.github.com>
2019-07-29 18:00:46 +0530

    Update MuzixRepository.java

--- src/main/java/com/stackroute/repository/MuzixRepository.java
+++ src/main/java/com/stackroute/repository/MuzixRepository.java
@@ -8,8 +8,11 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
+//Extending jpa repository to handle all database operationns
 public interface MuzixRepository extends JpaRepository<Muzix,Integer> {
+    //This is used to get the tracks by only giving the name
     @Query("SELECT t FROM Track t WHERE name = ?1")
+    //Method is implemented in the service
     List<Muzix> getTrackByName(String name);
 
 }
