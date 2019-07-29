a516a0d063606114c16007feb12714b8fc166a4c
sahithi-chilakamarri <51943021+sahithi-chilakamarri@users.noreply.github.com>
2019-07-29 17:59:32 +0530

    Update ControllerAdvice.java

--- src/main/java/com/stackroute/globalexceptionhandler/ControllerAdvice.java
+++ src/main/java/com/stackroute/globalexceptionhandler/ControllerAdvice.java
@@ -8,20 +8,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.Optional;
-
-public class ControllerAdvice {
+//This class is used to handle the exceptions with out writing them in the controller class
+public class ControllerAdvice { 
+    //Craeting response entity to display the message
     private ResponseEntity <VndErrors> error(final Exception exception, final HttpStatus httpStatus, final String logRef) {
+        //To display the exception message and information
         final String message = Optional.of(exception.getMessage()).orElse(exception.getClass().getSimpleName());
         return new ResponseEntity<>(new VndErrors(logRef, message), httpStatus);
     }
     //Handling the tracknot found exception
     @ExceptionHandler(TrackNotFoundException.class)
     public ResponseEntity < VndErrors > notFoundException(final TrackNotFoundException exception) {
+        //To display the exception message and information and httpstatus
         return error(exception, HttpStatus.NOT_FOUND, exception.getMessage().toString());
     }
     //Handling the trackalreadyexists exception
     @ExceptionHandler(TrackAlreadyExistsException.class)
     public ResponseEntity < VndErrors > alreadyExistsException(final TrackAlreadyExistsException exception) {
+        //To display the exception message and information and httpstatus
         return error(exception, HttpStatus.NOT_FOUND, exception.getMessage());
     }
 
