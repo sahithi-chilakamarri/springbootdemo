093a5e745457b425b640bf39a1fc883a93540f23
Sahithi <sahithimpc@gmail.com>
2019-08-02 15:33:54 +0530

    Commiting springboot assignment using mongodb

--- src/main/java/com/stackroute/repository/MuzixRepository.java
+++ src/main/java/com/stackroute/repository/MuzixRepository.java
@@ -3,13 +3,14 @@ package com.stackroute.repository;
 import com.stackroute.domain.Muzix;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 //Extending jpa repository to handle all database operationns
-public interface MuzixRepository extends JpaRepository<Muzix,Integer> {
+public interface MuzixRepository extends MongoRepository<Muzix,Integer> {
     //This is used to get the tracks by only giving the name
     @Query("SELECT t FROM Track t WHERE name = ?1")
     //Method is implemented in the service
