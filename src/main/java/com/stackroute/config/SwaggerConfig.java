c6390c8e3e9aac27d4d0327ba991ad23f6b05a45
sahithi-chilakamarri <51943021+sahithi-chilakamarri@users.noreply.github.com>
2019-08-06 17:29:11 +0530

    Update SwaggerConfig.java

--- src/main/java/com/stackroute/config/SwaggerConfig.java
+++ src/main/java/com/stackroute/config/SwaggerConfig.java
@@ -12,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
     @Bean
     public Docket productApi(){
+        // Using swagger 2
         return new Docket(DocumentationType.SWAGGER_2)
                 .select().apis(RequestHandlerSelectors.basePackage("com.stackroute"))
                 .build();
